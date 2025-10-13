9685197a37dbd0e0dffa46153f0d2ae3cd851177
ejra92 <ericrodrigueza123@gmail.com>
2025-10-13 02:35:31 -0300

    feat: fix test

--- src/features/searcher/searcher.test.tsx
+++ src/features/searcher/searcher.test.tsx
@@ -73,6 +73,21 @@ describe('Searcher suite tests', () => {
     expect(form).toHaveAttribute('action', '/planets/search');
   });
 
+  it('Should not redirect if inputText does not have value and enter key o submit button has been pressed', async () => {
+    const {
+      inputText,
+      button
+    } = renderSearcher();
+
+    expect(inputText).toHaveValue('');
+
+    await userEvent.click(button);
+
+    await userEvent.keyboard('{enter}');
+
+    expect(push).not.toHaveBeenCalled();
+  });
+
   it('Should redirect if inputText has a value and submit button has been pressed and', async () => {
     const {
       inputText,
@@ -108,20 +123,5 @@ describe('Searcher suite tests', () => {
       undefined,
       { scroll: undefined }
     );
-  });
-
-  it('Should not redirect if inputText does not have value and enter key o submit button has been pressed', async () => {
-    const {
-      inputText,
-      button
-    } = renderSearcher();
-
-    expect(inputText).toHaveValue('');
-
-    await userEvent.click(button);
-
-    await userEvent.keyboard('{enter}');
-
-    expect(push).not.toHaveBeenCalled();
   });
 });
